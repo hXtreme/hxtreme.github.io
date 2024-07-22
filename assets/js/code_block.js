de4d41f1cd53a6023e4ff32e5fe68fe59b7583c1
Harsh Parekh <harsh_parekh@outlook.com>
2024-07-22 08:49:28 +0000

    Simplify code-block copy mechanism
    Some day I will figure out how to place that copy
    button correctly, till then this will do.

--- assets/js/code_block.js
+++ assets/js/code_block.js
@@ -2,20 +2,17 @@ var body = document.getElementsByTagName('body')[0];
 
 
 /**
- * Place a copy button on code-block
+ * Copy code-blocks on click
  *
  * @param {HTMLElement} elem
  */
-function add_copy_button(elem) {
-  var copy_button = document.createElement("i");
-  copy_button.classList.add("fa", "fa-copy", "copy-button");
-  copy_button.onclick = function() {
+function copy_code_block_onclick(elem) {
+  elem.onclick = function() {
     navigator.clipboard.writeText(elem.innerText).then(function() {
       gen_toast("Snippet copied to clipboard", 2000);
     })
   }
-  elem.appendChild(copy_button);
 }
 
-// Place copy button on all code-blocks
-Array.from(document.getElementsByTagName("pre")).forEach(add_copy_button);
+// Add copy-onclick to all code-blocks
+Array.from(document.getElementsByTagName("pre")).forEach(copy_code_block_onclick);
